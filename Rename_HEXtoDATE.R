6f61674f0a47ff9af216473d614f215e8d259afa
JF <jfgodeau@gmail.com>
2018-07-23 01:02:28 +0200

    Update_Juillet2018

--- Rename_HEXtoDATE.R
+++ Rename_HEXtoDATE.R
@@ -1,6 +1,5 @@
 fchoose <- file.choose()
-slash <- gregexpr("/", fchoose)[[1]]
-setwd(substring(fchoose, 1, slash[length(slash)]))
+setwd(dirname(fchoose))
 #setwd("/media/jf/Elements/Audiomoth/20180416_Comparatif_indoor")
 arq <- dir(pattern="WAV") # store all wave files in the working directory
 x <- as.integer(as.hexmode(substr(arq, 1, 8))) # get the integer version of the HEX code
@@ -9,10 +8,10 @@ newname <- gsub("[ :]","_",newname)
 file.rename(from=arq, to=paste0(newname,".wav"))
 
 ######################## Manipulate names inside of Results.csv ############  
-fchoose <- file.choose()
-RBC <- read.csv(fchoose)
-x <- as.integer(as.hexmode(substr(RBC$FileName, 1, 8))) # get the integer version of the HEX code
-newname <- as.POSIXct(x, origin="1970-01-01")  # show the date and time of each file.
-newname <- gsub("[ :]","_",newname)
-RBC$FileName <- factor(newname)
-write.csv(RBC, fchoose)
+#fchoose <- file.choose()
+#RBC <- read.csv(fchoose)
+#x <- as.integer(as.hexmode(substr(RBC$FileName, 1, 8))) # get the integer version of the HEX code
+#newname <- as.POSIXct(x, origin="1970-01-01")  # show the date and time of each file.
+#newname <- gsub("[ :]","_",newname)
+#RBC$FileName <- factor(newname)
+#write.csv(RBC, fchoose)
